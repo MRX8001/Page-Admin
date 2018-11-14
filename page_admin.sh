7ed8fccfa07b0504d727db7ad9be02b80377d63b
MRX8001 <guiluiscom08@gmail.com>
2018-11-14 16:17:35 -0200

    new version

--- page_admin.sh
+++ page_admin.sh
@@ -10,11 +10,12 @@ figlet P-Admin
 #
 #
 echo "DIGITE A URL DO SITE (Ex:www.site.com ou site.com)"
+read -p "[+]=>" url
 
 for palavra in $(cat link.txt)
 do
 
-resposta=$(curl -s -o /dev/null -w "%{http_code}" $1/$palavra)
+resposta=$(curl -s -o /dev/null -w "%{http_code}" $url/$palavra)
 if [ $resposta == "200" ]
 then
 
